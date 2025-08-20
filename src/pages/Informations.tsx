06df2db3f608e493d3218b067709f659ce6c9599
kevinsg1997 <kevinsg1997@gmail.com>
2025-08-19 23:46:42 -0300

    fix - API Link

--- src/pages/Informations.tsx
+++ src/pages/Informations.tsx
@@ -11,7 +11,7 @@ export default function RSVPForm() {
     setStatus("Enviando...");
 
     try {
-      const response = await fetch("https://weddingwebsiteapi-production.up.railway.app/api/email/confirm", {
+      const response = await fetch("https://weddingwebsiteapi-production.up.railway.app/api/rsvp/confirm", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, attending }),
