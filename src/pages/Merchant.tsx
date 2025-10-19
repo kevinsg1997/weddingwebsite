efd5f920250ba7a16bc8ee844050649833881656
kevinsg1997 <kevinsg1997@gmail.com>
2025-10-19 18:43:45 -0300

    fix

--- src/pages/Merchant.tsx
+++ src/pages/Merchant.tsx
@@ -99,7 +99,7 @@ export default function Merchant() {
           {isLoadingItems ? (
             <p className="text-white">Carregando itens...</p>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div className="flex flex-wrap justify-center gap-6">
               {items.map((item) => (
                 <div
                   key={item.id}
